e1ac7104b722a2424d18420d3f0ed7c0ccd24d09
Chbee <djaaksmscjs@naver.com>
2025-12-18 21:14:35 +0900

    add: 기본 프로젝트 설정

---
+++ TravelCalculator/TravelCalculator/TravelCalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravelCalculatorApp.swift
+//  TravelCalculator
+//
+//  Created by 손지영 on 12/18/25.
+//
+
+import SwiftUI
+
+@main
+struct TravelCalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ TravelCalculator/TravelCalculatorTests/TravelCalculatorTests.swift
@@ -0,0 +1,17 @@
+//
+//  TravelCalculatorTests.swift
+//  TravelCalculatorTests
+//
+//  Created by 손지영 on 12/18/25.
+//
+
+import Testing
+@testable import TravelCalculator
+
+struct TravelCalculatorTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
